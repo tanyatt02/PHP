e6055dcf06bf9311e495a54d4dafc2a9bbcbb131
tanyatt02 <62353476+tanyatt02@users.noreply.github.com>
2021-07-20 17:28:43 +0300

    Delete DZ26_0 directory

--- DZ26_0/public/twig.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('/');
-$twig = new \Twig\Environment($loader);
-
-echo $twig->render('template.twig', ['name' => 'Fabien']);
